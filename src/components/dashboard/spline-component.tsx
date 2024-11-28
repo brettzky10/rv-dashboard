26c75254db82089aca72c695cbd769993857ad4d
brettzky10 <brett.linseman@gmail.com>
2024-11-28 06:17:12 -0500

    added rv model

--- src/components/dashboard/spline-component.tsx
+++ src/components/dashboard/spline-component.tsx
@@ -6,7 +6,10 @@ import Spline from "@splinetool/react-spline";
 export default function SplineComponent() {
   return (
     <div className="h-screen">
-      <Spline scene="https://prod.spline.design/r8n9DcRmCAX8oliX/scene.splinecode" />
+      {/* <Spline scene="https://prod.spline.design/r8n9DcRmCAX8oliX/scene.splinecode" /> */}
+      <Spline
+        scene="https://prod.spline.design/z9SoXInidUUn3uiv/scene.splinecode" 
+      />
     </div>
   );
 }
--- src/components/global/navbar/header.tsx
+++ src/components/global/navbar/header.tsx
@@ -5,13 +5,13 @@ export function Header() {
     <div className="flex flex-col justify-center md:items-start md:flex-row md:justify-between md:mt-3 md:mx-3">
       <div className="flex flex-col md:mt-0 text-center md:text-left ">
         <h1 className="text-xl md:text-4xl font-bold text-shadow-lg shadow-slate-800 uppercase">
-          Weather Station
+          RV Dashboard
         </h1>
         <div className="flex justify-center md:justify-start items-center gap-1">
           <Icons.currentLocation className="w-6 h-6 md:block"/>
 
           <p className="text-sm md:text-xl text-shadow shadow-slate-800 ">
-            California,Buena Park, CA 90621
+            Ontario, Canada
           </p>
         </div>
       </div>
